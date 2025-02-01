bc739f08726cb30ed22735a60932049f2655d614
Antonela Abicic <antonela.abicic@gmail.com>
2025-02-01 13:21:02 +0100

    refactored a bit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("kotlin-kapt")
+    id("com.google.secrets_gradle_plugin") version "0.4"
 }
 
 android {
